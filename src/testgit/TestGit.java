26ff5f09ad55a1183c543738d754d4bbeb3debb9
famontecillo <famontecillo@DESKTOP-9DMRF3C>
2022-05-24 16:48:50 -0500

    Inicio del proyecto GIT

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author famontecillo
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Este es un ejmplo de git control de versiones!!");
+    }
+    
+}
